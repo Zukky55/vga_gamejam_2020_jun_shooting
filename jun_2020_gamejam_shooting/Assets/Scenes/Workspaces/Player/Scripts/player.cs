22b4ef1d348c9651811710ff310db927a764c755
KazukiTakahashi0213 <57526865+KazukiTakahashi0213@users.noreply.github.com>
2020-01-25 15:01:14 +0900

    playerの作成

---
+++ jun_2020_gamejam_shooting/Assets/Scenes/Workspaces/Player/Scripts/player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace gamejam {
+
+	public class player : MonoBehaviour {
+		[SerializeField]
+		private float spead = 0.1f;
+		[SerializeField]
+		Rigidbody2D rb2d;
+
+		public float Horizontal => Input.GetAxis("Horizontal");
+		public float Vertical => Input.GetAxis("Vertical");
+		public Vector3 Velocity => new Vector3(Horizontal * spead, Vertical * spead, 0f);
+
+		void Start() {
+			rb2d = GetComponent<Rigidbody2D>();
+		}
+
+		void Update() {
+			if (Velocity != Vector3.zero) {
+				Move();
+			}
+		}
+
+		private void Move() {
+			//transform.position += Velocity;
+			rb2d.transform.position += Velocity;
+		}
+	}
+}
