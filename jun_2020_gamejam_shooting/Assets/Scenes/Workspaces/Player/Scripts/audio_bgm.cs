a6f492e13ad8b7e445675048aa2cab3fd92e1b07
KazukiTakahashi0213 <57526865+KazukiTakahashi0213@users.noreply.github.com>
2020-01-26 15:04:27 +0900

    t

---
+++ jun_2020_gamejam_shooting/Assets/Scenes/Workspaces/Player/Scripts/audio_bgm.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace gamejam {
+    public class audio_bgm : MonoBehaviour {
+        [SerializeField]
+        private AudioClip game_bgm;
+        [SerializeField]
+        private AudioClip title_bgm;
+        AudioSource _audioSource;
+
+        void Start() {
+            _audioSource = GetComponent<AudioSource>();
+            GameManager.Instance.Statemachine.SubscribeEvent(When.Enter, title_bgm_start);
+            GameManager.Instance.Statemachine.SubscribeEvent(When.Exit, title_bgm_end);
+            GameManager.Instance.Statemachine.SubscribeEvent(When.Enter, game_bgm_start);
+            GameManager.Instance.Statemachine.SubscribeEvent(When.Exit, game_bgm_end);
+        }
+
+        void Update() {
+        }
+
+        void title_bgm_start(State sample) {
+            if (!sample.Equals(State.Title)) return;
+            _audioSource.clip = title_bgm;
+            _audioSource.Play();
+
+        }
+        void title_bgm_end(State sample) {
+            if (!sample.Equals(State.Title)) return;
+            _audioSource.Stop();
+        }
+
+        void game_bgm_start(State sample) {
+            if (!sample.Equals(State.InGame)) return;
+            _audioSource.clip = game_bgm;
+            _audioSource.Play();
+
+        }
+        void game_bgm_end(State sample) {
+            if (!sample.Equals(State.InGame)) return;
+            _audioSource.Stop();
+        }
+    }
+}
---
+++ jun_2020_gamejam_shooting/Assets/Scenes/Workspaces/Player/Scripts/audio_se.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace gamejam {
+	public class audio_se : MonoSingleton<audio_se> {
+
+		[SerializeField]
+		private AudioClip victry_se;
+		[SerializeField]
+		private AudioClip countDown_se;
+		[SerializeField]
+		private AudioClip damage_se;
+		[SerializeField]
+		private AudioClip startButton_se;
+		[SerializeField]
+		private AudioClip start_se;
+		[SerializeField]
+		private AudioClip button_se;
+		[SerializeField]
+		private AudioClip bullet_se;
+		[SerializeField]
+		private AudioClip roseBomb_se;
+		AudioSource _audioSource;
+
+		static audio_se instance;
+
+		void Update() {
+		}
+
+		void Start() {
+			_audioSource = GetComponent<AudioSource>();
+			GameManager.Instance.Statemachine.SubscribeEvent(When.Enter, victry_se_start);
+			GameManager.Instance.Statemachine.SubscribeEvent(When.Enter, victry_se_start);
+		}
+
+		void instance_start(State sample) {
+			if (!sample.Equals(State.InGame)) return;
+			instance = new audio_se();
+		}
+
+		void victry_se_start(State sample) {
+			if (!sample.Equals(State.InGame)) return;
+			_audioSource.PlayOneShot(victry_se);
+		}
+
+
+	}
+}
