7df2a81904524e161edb4e4396904346ff6445b6
Zukky_13 <www.kase@gmail.com>
2020-01-26 12:49:35 +0900

    no message

--- jun_2020_gamejam_shooting/Assets/Scripts/CountDown.cs
+++ jun_2020_gamejam_shooting/Assets/Scripts/CountDown.cs
@@ -1,19 +1,52 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace gamejam
 {
     public class CountDown : MonoBehaviour
     {
+        [SerializeField]
+        TimeParameter param;
+
+        Timer timer;
+
         private void Awake()
         {
-            GameManager.Instance.Statemachine.SubscribeEvent(When.Enter, OnCountdownEnter);
+
+            GameManager.Instance.Statemachine.SubscribeEvent(When.Enter, OnStateEnter);
+            GameManager.Instance.Statemachine.SubscribeEvent(When.Exit, OnStateExit);
         }
 
-        private void OnCountdownEnter(State state)
+        private void OnStateEnter(State obj)
         {
-            if (state.Equals(State.CountDown)) return;
-            // tds
+            if (!obj.Equals(State.CountDown)) return;
+        }
+
+        private void OnStateExit(State obj)
+        {
+            if (!obj.Equals(State.CountDown)) return;
         }
     }
+    public class ResultState : MonoBehaviour
+    {
+
+        [SerializeField]
+        GameObject resultWindow;
+        private void Awake()
+        {
+
+            GameManager.Instance.Statemachine.SubscribeEvent(When.Enter, OnStateEnter);
+            GameManager.Instance.Statemachine.SubscribeEvent(When.Exit, OnStateExit);
+        }
+
+        private void OnStateEnter(State obj)
+        {
+            if (!obj.Equals(State.Result)) return;
+        }
+
+        private void OnStateExit(State obj)
+        {
+            if (!obj.Equals(State.Result)) return;
+        }
+    }
+
 }
--- jun_2020_gamejam_shooting/Assets/Scripts/TitleState.cs
+++ jun_2020_gamejam_shooting/Assets/Scripts/TitleState.cs
@@ -29,4 +29,5 @@ namespace gamejam
             title.SetActive(false);
         }
     }
+
 }
