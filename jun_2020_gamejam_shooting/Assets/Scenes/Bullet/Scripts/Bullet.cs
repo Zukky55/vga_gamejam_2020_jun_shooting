00c49ac7e71f3ce9cf9f68e8f8751343ae77d10e
Zukky_13 <www.kase@gmail.com>
2020-01-26 13:12:18 +0900

    no message

--- jun_2020_gamejam_shooting/Assets/Scenes/Bullet/Scripts/Bullet.cs
+++ jun_2020_gamejam_shooting/Assets/Scenes/Bullet/Scripts/Bullet.cs
@@ -5,80 +5,93 @@ using UnityEngine;
 using UniRx;
 using UniRx.Triggers;
 
-namespace gamejam {
+namespace gamejam
+{
 
-	[RequireComponent(typeof(Rigidbody), typeof(Collider2D))]
-	public class Bullet : MonoBehaviour {
-		[SerializeField]
-		Rigidbody2D rb2d;
+    [RequireComponent(typeof(Rigidbody), typeof(Collider2D))]
+    public class Bullet : MonoBehaviour
+    {
+        [SerializeField]
+        Rigidbody2D rb2d;
 
-		[SerializeField]
-		BulletParam param;
+        [SerializeField]
+        BulletParam param;
 
-		bool isInitialized = false;
-		bool isUsing = false;
-		Vector3 velocity;
-		Vector3 spawnPos;
+        bool isInitialized = false;
+        bool isUsing = false;
+        Vector3 velocity;
+        Vector3 spawnPos;
 
-		const float destroyBoundary = 1000f;
+        const float destroyBoundary = 1000f;
 
-		public bool IsUsing { get => isUsing; set => isUsing = value; }
-		public BulletParam Param { get => param; set => param = value; }
+        public bool IsUsing { get => isUsing; set => isUsing = value; }
+        public BulletParam Param { get => param; set => param = value; }
 
-		private void Start() {
-			this.UpdateAsObservable()
-				.Where(_ => isUsing && isInitialized)
-				.Subscribe(_ => {
-					Move();
-				})
-				.AddTo(this);
+        private void Start()
+        {
+            this.UpdateAsObservable()
+                .Where(_ => isUsing && isInitialized)
+                .Subscribe(_ =>
+                {
+                    Move();
+                })
+                .AddTo(this);
 
-			this.UpdateAsObservable()
-				.Where(_ => (transform.position - spawnPos).sqrMagnitude > destroyBoundary)
-				.Subscribe(_ => {
-					Deactivate();
-				})
-				.AddTo(this);
-		}
+            this.UpdateAsObservable()
+                .Where(_ => (transform.position - spawnPos).sqrMagnitude > destroyBoundary)
+                .Subscribe(_ =>
+                {
+                    Deactivate();
+                })
+                .AddTo(this);
+        }
 
 
-		void Move() {
-			rb2d.velocity = velocity;
-			transform.up = velocity;
-		}
+        void Move()
+        {
+            rb2d.velocity = velocity;
+            transform.up = velocity;
+        }
 
-		public void Shot(Vector3 spawnPos, Vector3 velocity) {
-			this.spawnPos = spawnPos;
-			transform.position = spawnPos;
-			if (!gameObject.activeSelf) gameObject.SetActive(true);
-			this.velocity = velocity;
-			isInitialized = true;
-			isUsing = true;
-		}
+        public void Shot(Vector3 spawnPos, Vector3 velocity)
+        {
+            this.spawnPos = spawnPos;
+            transform.position = spawnPos;
+            if (!gameObject.activeSelf) gameObject.SetActive(true);
+            this.velocity = velocity;
+            isInitialized = true;
+            isUsing = true;
+        }
 
-		public void Deactivate() {
-			gameObject.SetActive(false);
-			isInitialized = false;
-			isUsing = false;
-		}
+        public void Deactivate()
+        {
+            gameObject.SetActive(false);
+            isInitialized = false;
+            isUsing = false;
+        }
 
-		private void OnCollisionEnter2D(Collision2D collision) {
-			Debug.Log($"collied name is {gameObject.name}");
-			IOnwer owner = null;
-			if (collision.gameObject.TryGetComponent(out owner)) {
-				if (!owner.Type.Equals(param.Type)) {
-					owner.TakeDamage(param.Atatck);
-					if (owner.HP <= 0) {
-						owner.Destroy();
-					}
-				}
-				Deactivate();
-			}
-		}
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            Debug.Log($"collied name is {gameObject.name}");
+            IOnwer owner = null;
+            if (collision.gameObject.TryGetComponent(out owner))
+            {
+                if (!owner.Type.Equals(param.Type))
+                {
+                    owner.TakeDamage(param.Atatck);
+                    if (owner.HP <= 0)
+                    {
+                        owner.Destroy();
+                    }
+                    Deactivate();
+                }
+            }
+        }
 
-		private void OnTriggerEnter2D(Collider2D collision) {
-			Debug.Log($"triggered {collision.gameObject.name}");
-		}
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            Debug.Log($"triggered {collision.gameObject.name}");
+        }
 
-	}
+    }
 }
