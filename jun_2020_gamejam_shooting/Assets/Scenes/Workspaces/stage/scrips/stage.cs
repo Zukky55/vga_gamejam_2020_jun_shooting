a072c759ba43a5dba0925bc1bfc99c67320de1e7
KazukiTakahashi0213 <57526865+KazukiTakahashi0213@users.noreply.github.com>
2020-01-26 10:43:01 +0900

    stage途中

---
+++ jun_2020_gamejam_shooting/Assets/Scenes/Workspaces/stage/scrips/stage.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace gamejam {
+
+    public class stage : MonoBehaviour {
+        void Start() {
+
+        }
+
+        void Update() {
+
+        }
+
+        void OnTriggerExit2D(Collider2D other) {
+            if (other.gameObject.CompareTag("Player")) {
+                other.gameObject.SetActive(false);
+            }
+            if (other.gameObject.CompareTag("Bullet")) {
+                other.gameObject.SetActive(false);
+            }
+
+            //Bullet b = other.GetComponent<Bullet>();
+            //b.Diactivate(true);
+            //hp -= 1;
+
+        }
+
+    }
+}
